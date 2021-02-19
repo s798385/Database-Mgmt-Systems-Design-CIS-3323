c25a2d5ee928c77b5eed03fdde5b68f811e22723
s798385 <69857146+s798385@users.noreply.github.com>
2021-02-19 12:05:35 -0600

    Update movies.sql

--- SQL Databases/movies.sql
+++ SQL Databases/movies.sql
@@ -1,43 +1,58 @@
-/* Delete the tables if they already exist */
 drop table if exists Movie;
 drop table if exists Reviewer;
 drop table if exists Rating;
 
-/* Create the schema for our tables */
-create table Movie (mID text, title text, year text, director text);
-create table Reviewer (rID text, name text);
-create table Rating (rID text, mID text, stars text, ratingDate text);
+CREATE TABLE `Movie` (
+  `mID` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `year` year(4) NOT NULL,
+  `directory` varchar(256) DEFAULT NULL
+);
 
-/* Populate the tables with our data */
-insert into Movie values ("101","Gone with the wind","1939","Victor Fleming");
-insert into Movie values ("102","Star Wars", "1977", "George Lucas");
-insert into Movie values ("103","The Sound of Music","1965","Robert Wise");
-insert into Movie values ("104","E.T.","1982","Steven Spielberg");
-insert into Movie values ("105","Titanic","1997","James Cameron");
-insert into Movie values ("106","Snow White","1937","");
-insert into Movie values ("107","Avatar","2009","James Cameron");
-insert into Movie values ("108","Raiders of the Lost Ark","1981","Steven Spielberg");
- 
-insert into Reviewer values ("201","Sarah Martinez");
-insert into Reviewer values ("202","Daniel Lewis");
-insert into Reviewer values ("203","Brittany Harris");
-insert into Reviewer values ("204","Mike Anderson");
-insert into Reviewer values ("205","Chris Jackson");
-insert into Reviewer values ("206","Elizabeth Thomas");
-insert into Reviewer values ("207","James Cameron");
-insert into Reviewer values ("208","Ashley White");
- 
-insert into Rating values ("201","101","2","2011-01-22");
-insert into Rating values ("201","101","4","2011-01-27");
-insert into Rating values ("202","106","4","");
-insert into Rating values ("203","103","2","2011-01-20");
-insert into Rating values ("203","108","4","2011-01-12");
-insert into Rating values ("203","108","2","2011-01-30");
-insert into Rating values ("204","101","3","2011-01-09");
-insert into Rating values ("205","103","3","2011-01-27");
-insert into Rating values ("205","104","2","2011-01-22");
-insert into Rating values ("205","108","4","");
-insert into Rating values ("206","107","3","2011-01-15");
-insert into Rating values ("206","106","5","2011-01-19");
-insert into Rating values ("207","107","5","2011-01-20");
-insert into Rating values ("208","104","3","2011-01-02");
+INSERT INTO `Movie` (`mID`, `title`, `year`, `directory`) VALUES
+(101, 'Gone with the Wind', 1939, 'Victor Fleming'),
+(102, 'Star Wars', 1977, 'George Lucas'),
+(103, 'The Sound of Music', 1965, 'Robert Wise'),
+(104, 'E.T.', 1982, 'Steven Spielberg'),
+(105, 'Titanic', 1997, 'James Cameron'),
+(106, 'Snow White', 1937, NULL),
+(107, 'Avatar', 2009, 'James Cameron'),
+(108, 'Raiders of the Lost Ark', 1981, 'Steven Spielberg');
+
+CREATE TABLE `Rating` (
+  `rID` int(11) NOT NULL,
+  `mID` int(11) NOT NULL,
+  `stars` int(11) DEFAULT NULL,
+  `ratingDate` date DEFAULT NULL
+);
+
+INSERT INTO `Rating` (`rID`, `mID`, `stars`, `ratingDate`) VALUES
+(201, 101, 2, '2011-01-22'),
+(201, 101, 4, '2011-01-27'),
+(202, 106, 4, NULL),
+(203, 103, 2, '2011-01-20'),
+(203, 108, 4, '2011-01-12'),
+(203, 108, 2, '2011-01-30'),
+(204, 101, 3, '2011-01-09'),
+(205, 103, 3, '2011-01-27'),
+(205, 104, 2, '2011-01-22'),
+(205, 108, 4, NULL),
+(206, 107, 3, '2011-01-15'),
+(206, 106, 5, '2011-01-19'),
+(207, 107, 5, '2011-01-20'),
+(208, 104, 3, '2011-01-02');
+
+CREATE TABLE `Reviewer` (
+  `rID` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL
+);
+
+INSERT INTO `Reviewer` (`rID`, `name`) VALUES
+(201, 'Sarah Martinez'),
+(202, 'Daniel Lewis'),
+(203, 'Brittany Harris'),
+(204, 'Mike Anderson'),
+(205, 'Chris Jackson'),
+(206, 'Elizabeth Thomas'),
+(207, 'James Cameron'),
+(208, 'Ashley White');
